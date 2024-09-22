8245dec6081995036ca0271d3fbbf178a9afd047
James <jamesid2915@gmail.com>
2024-09-22 23:46:03 +0100

    fixed vercel kinde issue

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     experimental: {
       missingSuspenseWithCSRBailout: false,
     },
-  }
-  const nextConfig = {
     env: {
       KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
-      KINDE_POST_LOGOUT_REDIRECT_URL:
-        process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
-      KINDE_POST_LOGIN_REDIRECT_URL:
-        process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/dashboard`
+      KINDE_POST_LOGOUT_REDIRECT_URL: process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
+      KINDE_POST_LOGIN_REDIRECT_URL: process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/dashboard`
     }
   };
   
